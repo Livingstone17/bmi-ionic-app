cde2d700a356d6dcc55c9a16db4720b0093bb427
Peter Samson <peter.samson@xpresspayments.com>
2021-02-05 09:33:16 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React, {useRef,useState} from 'react';
+// import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
+  
+  IonContent,
+  IonGrid,
+  IonHeader,
   IonIcon,
+  IonInput,
+  IonItem,
   IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  IonAlert,
+  IonCol,
 } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import BmiControls from './components/BmiControls'
+import BmiResult from './components/BmiResult'
+import InputControl from './components/InputControl'
+
+// import { IonReactRouter } from '@ionic/react-router';
+// import { ellipse, square, triangle } from 'ionicons/icons';
+// import Tab1 from './pages/Tab1';
+// import Tab2 from './pages/Tab2';
+// import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,33 +45,79 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+  const [calculatedBmi, setCalculatedBmi]=useState<number>();
+  const [error, setError]=useState<string>();
+  const [calcUnits, setCalcUnits]=useState<'mkg' | 'ftlbs'>('mkg');
+  const weightInputRef=useRef<HTMLIonInputElement>(null);
+  const heightInputRef=useRef<HTMLIonInputElement>(null);
+
+
+  const calculateBMI = () =>{
+const enteredWeight= weightInputRef.current!.value;
+const enteredHeight= heightInputRef.current!.value;
+
+if(!enteredHeight || !enteredWeight || +enteredWeight <=0 || +enteredHeight<=0){
+  setError('please enter a valid (non-negative) number')
+  return;
+}
+
+const weightConversionFactor =  calcUnits === 'ftlbs' ? 2.2 : 1;
+const heightConversionFactor =  calcUnits === 'ftlbs' ? 3.28 : 1;
+const weight = +enteredWeight / weightConversionFactor;
+const height = +enteredHeight /heightConversionFactor;
+const bmi = weight/(height* height)
+
+console.log(bmi)
+setCalculatedBmi(bmi);
+  };
+
+  const resetInputs =() =>{
+    weightInputRef.current!.value="";
+    heightInputRef.current!.value="";
+  };
+
+  const clearError=()=>{
+    setError('');
+  }
+
+  const selectCalcUnitHandler=(selectedValue:'mkg'|'ftlbs') =>{
+    setCalcUnits(selectedValue);
+  }
+return(
+  <>
+  <IonAlert isOpen={!!error} message={error} buttons={[{text:'Okay', handler:clearError}]}/>
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>BMI Calculator</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className="ion-padding">
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <InputControl selectedValue={calcUnits} onSelectValue={selectCalcUnitHandler} />
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      <IonItem>
+        <IonLabel position="floating">Your height ({calcUnits==='mkg' ? 'meters' : 'feet'})</IonLabel>
+        <IonInput type="number" ref={heightInputRef}></IonInput>
+      </IonItem>
+      <IonItem>
+        <IonLabel position="floating">Your weight ({calcUnits==='mkg' ? 'kg' : 'lbs'})</IonLabel>
+        <IonInput type="number" ref={weightInputRef}></IonInput>
+      </IonItem>
+      <IonGrid>
+        <BmiControls onCalculate={calculateBMI} onReset={resetInputs}/>
+        {calculatedBmi && (
+          <BmiResult result={calculatedBmi}/>
+        )}
+      </IonGrid>
+    </IonContent>
   </IonApp>
-);
+  </>
+)};
 
 export default App;
---
+++ src/components/BmiControls.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+    IonApp,
+    IonButton,
+    IonCol,
+   
+    IonIcon,
+    
+    IonRow,
+    
+  } from '@ionic/react';
+  import {calculatorOutline, refreshOutline} from 'ionicons/icons';
+
+
+const BmiControls: React.FC<{onCalculate:()=>void; onReset:()=>void;}>=(props) =>{
+    return(
+        
+        <IonRow>
+          <IonCol className="ion-text-left">
+            <IonButton onClick={props.onCalculate}>
+              <IonIcon slot="start" icon={calculatorOutline} /> Calculate</IonButton>
+          </IonCol>
+          <IonCol className="ion-text-right">
+            <IonButton fill="outline"  onClick={props.onReset}>
+            <IonIcon slot="start" icon={refreshOutline}/>Reset</IonButton>
+          </IonCol>
+        </IonRow>
+    )
+}
+
+export default BmiControls;
